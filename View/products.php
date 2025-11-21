e3b991c8f8aabc23806d3ddc96a5824048a25400
Bogdanchik <holubiev.bohdan1122@vu.cdu.edu.ua>
2025-11-21 16:14:52 +0200

    Update products.php

--- View/products.php
+++ View/products.php
@@ -1,29 +1,23 @@
 <?php
-$products = [
-    ['id'=>1,'name'=>'Маргарита','price'=>120,'img'=>'images/margherita.jpg'],
-    ['id'=>2,'name'=>'Пепероні','price'=>150,'img'=>'images/peperoni.jpg'],
-    ['id'=>3,'name'=>'Кватро формаджі','price'=>180,'img'=>'images/quattro.jpg'],
-    ['id'=>4,'name'=>'Гавайська','price'=>160,'img'=>'images/hawaiian.jpg'],
-    ['id'=>5,'name'=>'Мисливська','price'=>170,'img'=>'images/hunter.jpg'],
-    ['id'=>6,'name'=>'Вегетаріанська','price'=>140,'img'=>'images/veggie.jpg'],
-    ['id'=>7,'name'=>'Чотири сира','price'=>190,'img'=>'images/fourcheese.jpg'],
-    ['id'=>8,'name'=>'Мексикана','price'=>155,'img'=>'images/mexicana.jpg'],
-    ['id'=>9,'name'=>'Барбекю','price'=>165,'img'=>'images/bbq.jpg'],
-    ['id'=>10,'name'=>'Франческа','price'=>175,'img'=>'images/francesca.jpg'],
-    ['id'=>11,'name'=>'Тунцова','price'=>160,'img'=>'images/tuna.jpg'],
-    ['id'=>12,'name'=>'Рікотта','price'=>185,'img'=>'images/ricotta.jpg'],
-    ['id'=>13,'name'=>'Діабло','price'=>170,'img'=>'images/diablo.jpg'],
-    ['id'=>14,'name'=>'Маринара','price'=>130,'img'=>'images/marinara.jpg'],
-    ['id'=>15,'name'=>'Салямі','price'=>150,'img'=>'images/salami.jpg']
-];
 
-$drinks = [
-    ['key'=>'cola','name'=>'Кола','price'=>25],
-    ['key'=>'sprite','name'=>'Спрайт','price'=>25],
-    ['key'=>'fanta','name'=>'Фанта','price'=>25],
-    ['key'=>'water','name'=>'Вода','price'=>15],
-    ['key'=>'blacktea','name'=>'Чорний чай','price'=>20],
-    ['key'=>'greentea','name'=>'Зелений чай','price'=>20]
-];
+$url = "https://6920637531e684d7bfccd5fa.mockapi.io/api/pizzav1/Product";
 
+$response = file_get_contents($url);
+
+if ($response === false) {
+    die("Помилка отримання даних з MockAPI");
+}
+
+$data = json_decode($response, true);
+
+$products = [];
+$drinks = [];
+
+foreach ($data as $item) {
+    if (isset($item["isPizza"]) && $item["isPizza"] === true) {
+        $products[] = $item;
+    } else {
+        $drinks[] = $item;
+    }
+}
 return null;
