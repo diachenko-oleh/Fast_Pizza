bc41cc04cd21aec159536deb6609ddd101ae9b1a
diachenko-oleh <diachenko.oleh1122@vu.cdu.edu.ua>
2025-11-30 20:00:56 +0200

    Update admin_page.php

--- View/admin_page.php
+++ View/admin_page.php
@@ -29,7 +29,7 @@ $msg = $_GET['msg'] ?? '';
 				<div class="admin-title">Панель адміністратора</div>
 			</div>
 			<div>
-				<a href="index.php" class="btn btn-outline-secondary">Перейти до сайту</a>
+				<a href="menu_php.php" class="btn btn-outline-secondary">Перейти до сайту</a>
 			</div>
 		</div>
 
